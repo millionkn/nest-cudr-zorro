2b6b6e7e20c2ef7490706397a5b76a1964942fe4
millionkn <millionkn@sina.com>
2020-11-11 15:43:15 +0900

    number-editor

--- src/app/modal-editor/editors/number-editor/number-editor.component.ts
+++ src/app/modal-editor/editors/number-editor/number-editor.component.ts
@@ -22,10 +22,12 @@ export class NumberEditorComponent implements Editor<number, {}>, OnInit {
     max?: number,
     allowFloat: boolean,
   }) {
-    this.min = params.min;
-    this.max = params.max;
+    this.min = params.min || 0;
+    this.max = params.max || Number.MAX_SAFE_INTEGER;
     if (!params.allowFloat) {
       this.step = 1;
+    } else {
+      this.step = 0.1;
     }
   }
   valueChangeHandler(value: number) {
